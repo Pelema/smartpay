053dd14a1a13f38a2355825fd77b292cdf15540a
Pelema <abpalema@gmail.com>
2021-04-16 07:10:21 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const jwt = require("express-jwt");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+// const JWT_SECRET = require("./constants");
+var mysql      = require('mysql');
+const app = express();
+
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'me',
+//   password : 'secret',
+//   database : 'my_db'
+// });
+ 
+// connection.connect(function(err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     }
+   
+//     console.log('connected as id ' + connection.threadId);
+//   });
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+ 
+// connection.end();
+
+
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+
+app.use('/', express.static('views'));
+
+
+const auth = jwt({
+    secret: "JWT_SECRET",
+    algorithms: ['RS256'],
+    credentialsRequired: false,
+});
+app.use(auth);
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: {
+        endpoint: "/graphql",
+    },
+    context: ({ req }) => {
+        const user = req.headers.user
+            ? JSON.parse(req.headers.user)
+            : req.user
+            ? req.user
+            : null;
+        return { user };
+    },
+});
+
+server.applyMiddleware({ app });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("The server started on port " + PORT);
+});
