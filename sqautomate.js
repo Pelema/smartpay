0b5a8c6c3cece802b053c3501226243270ec9a31
Pelema <abpalema@gmail.com>
2022-03-24 13:16:05 +0200

    change admin credentials

--- sqautomate.js
+++ sqautomate.js
@@ -1,1 +1,1 @@
-sequelize-auto -h 'smartdb.casy0dqe9tjt.us-east-2.rds.amazonaws.com' -d 'smartstore' -u 'admin' -x 'smart9812' -p 3306  --dialect 'mysql' -o './models'
+sequelize-auto -h 'db-mysql-nyc3-34732-smartpaydb-do-user-11131132-0.b.db.ondigitalocean.com' -d 'smartstore' -u 'admin' -x 'vut3gNNH2GRejq82' -p 25060  --dialect 'mysql' -o './models'
